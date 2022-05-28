9cbfc12049b815bc018415eced43c44a321f6747
facundoarevalos04 <facundoarevalos04@gmail.com>
2022-05-27 23:05:41 -0300

    modifique mar

--- mar.js
+++ mar.js
@@ -1,5 +1,2 @@
-<<<<<<< HEAD
 console.log("el mar es inmenso","y tambien profundo", "azul eh imponente");
-=======
 console.log("el mar es inmenso","y tambien profundo","y contiene muchos seres acuaticos");
->>>>>>> 8c97465e76fdeb5060f716e49cccec4b78a14bc5
